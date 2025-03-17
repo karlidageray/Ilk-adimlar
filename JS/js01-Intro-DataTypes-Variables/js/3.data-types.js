4f27cf84930e0516fce6a7957992ec419e8bc2a4
karlidageray <karlidageray@gmail.com>
2025-03-17 17:47:58 +0300

    data const let

--- JS/js01-Intro-DataTypes-Variables/js/3.data-types.js
+++ JS/js01-Intro-DataTypes-Variables/js/3.data-types.js
@@ -10,3 +10,38 @@ console.log(typeof x);
 
 // 1111Number data type
 
+yas=40
+sayi=56.87
+console.log(yas);
+
+sonuc=(sayi*5)+6
+console.log(sonuc);
+
+const pi=Math.PI
+console.log(pi);
+
+
+// Ondalikli kismi duzenlemek icin kullanilan yontemler
+// round
+let yuvarlanmisSonuc=Math.round(sonuc)
+console.log(yuvarlanmisSonuc);
+
+// truch
+let truncSonuc=Math.trunc (sonuc)
+console.log(truncSonuc);
+
+// virgulden sonra gelen ondalikli kisimda kac hane gostermesini isteiyorsaniz toFixed (sayi)
+
+let fixedsonuc= sonuc.toFixed (2)
+console.log(fixedsonuc);
+
+
+// RANDOM metodu
+// rastgele sayi uretmek icin kullanilir
+// 0-1 arasinda bir sayi uretir
+
+console.clear()
+console.log(Math.round(Math.random()*100))
+
+console.log(Math.pow(5,3));
+console.log(Math.sqrt(144))
