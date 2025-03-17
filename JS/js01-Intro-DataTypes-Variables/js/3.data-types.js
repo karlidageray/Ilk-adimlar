54c3d9fa47632b165f93304964418baa1c8ce91f
karlidageray <karlidageray@gmail.com>
2025-03-17 18:35:40 +0300

    data type devami

--- JS/js01-Intro-DataTypes-Variables/js/3.data-types.js
+++ JS/js01-Intro-DataTypes-Variables/js/3.data-types.js
@@ -31,6 +31,7 @@ let truncSonuc=Math.trunc (sonuc)
 console.log(truncSonuc);
 
 // virgulden sonra gelen ondalikli kisimda kac hane gostermesini isteiyorsaniz toFixed (sayi)
+// Bir sayiya ToFixed uygulandiginda sonucu stringdir. 
 
 let fixedsonuc= sonuc.toFixed (2)
 console.log(fixedsonuc);
@@ -45,3 +46,30 @@ console.log(Math.round(Math.random()*100))
 
 console.log(Math.pow(5,3));
 console.log(Math.sqrt(144))
+console.clear()
+
+// String Data Type
+
+ad='eray'
+
+let str1='Merhaba dunya bugun \"17 ekim"'
+let str2="merhaba dunya"
+let str3=`merhaba ${ad}`
+let str4=new String("merhaba dunya")
+console.log(str1, str2, str3);
+console.log(str4);
+console.log(typeof str4);
+
+console.log(str1[4]);
+
+// string bu sekilde degistirilemez. 
+// stringler immutabledir/ 
+str1[4]= 'u'
+str1="Merhaba Mars"
+console.log(str1);
+
+let kimlik=12312312322
+let adsoyad='eray kar'
+
+console.log("Personelimiz", adsoyad, "in Kimlik Numarasi", kimlik)
+console.log(`Personelimiz ${adsoyad}in Kimlik Numarasi ${kimlik}`)
