843464133d777c98cad146ec8e11668653e5b509
karlidageray <karlidageray@gmail.com>
2025-03-18 01:48:45 +0300

    operators

---
+++ JS/js01-Intro-DataTypes-Variables/js/operators.js
@@ -0,0 +1,79 @@
+// OPERTORS
+
+
+// ARITMETIC OPERTORS
+
+let a=10
+let b=2
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%3);
+
+let result=365/6
+console.log(Math.trunc (result));
+console.log(result.toFixed(3));
+console.log(typeof result.toFixed());
+console.clear()
+// INCREMENT/DECREMENT
+
+// POST VE PRE OLARAK ADLANDIRILIR
+
+let x=5
+
+console.log(x++);
+console.log(x);
+
+console.log(x--);
+console.log(x);
+
+console.log(++x);
+console.log(--x);
+console.clear()
+// ASSIGNMENT OPERATORS
+
+let t=10
+let y=5
+
+
+console.log(t);
+console.log(y);
+
+console.log(t=t+y);
+
+console.log(t+=y);
+console.log(t-=y);
+
+let sayi=45.967
+console.log(sayi=Math.trunc(sayi)*2);
+console.log(sayi);
+
+// Comparison Operations
+
+let c=2
+let d='5'
+
+console.log(c==d);
+console.log(c===d);
+
+console.log(c!=d);
+console.clear()
+// logical operators 
+
+console.log(Boolean('merhaba'));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(2));
+console.log(Boolean(undefined));
+console.log(Boolean([]));
+
+console.log([] && 0 && 2);
+console.log([] && 'M' && 5);
+
+// || OR operators
+
+console.log(0 || null || 2 || undefined);
+
+
