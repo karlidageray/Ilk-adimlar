52cdbab664bfc4693551c97f6ca098f4fb497d14
karlidageray <karlidageray@gmail.com>
2025-03-18 14:57:30 +0300

    number formats

---
+++ JS/Js-03-conditions/conditions.js
@@ -0,0 +1,13 @@
+console.log("*** CONDITIONS ***");
+
+// const num1 = + prompt ('Bir sayi giriniz')
+const num1 = Number(prompt ('Bir sayi giriniz') ) 
+
+console.log(num1, typeof num1);
+
+const num2 = 5
+console.log(num2, typeof num2);
+
+console.log('Toplam:', num1+num2); //? String birlestirme (Concatination)
+
+
