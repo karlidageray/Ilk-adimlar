b00a84d5d89c8b87864bf48019c2eee80a586811
karlidageray <karlidageray@gmail.com>
2025-03-19 19:08:20 +0300

    donguler

---
+++ JS/JS-04-loops/for.js
@@ -0,0 +1,27 @@
+//? 50 kisinin notunu girerek ortalamasini hesaplayan kodu yaziniz
+
+// let sum=0
+// let i=0
+
+// for (i ; i<5; i++) {
+
+//     const notlar= Number (prompt(`${i+1}. notunu giriniz:`))
+//     sum= sum+notlar
+//     console.log(sum);
+// }
+
+// console.log(`AVG: ${sum/ i} `)
+
+//? Aralik verme
+
+const n1=3
+const n2=5
+let sum=0
+
+if (n1>n2) {
+    console.log("n2 should be bigger than n1")
+} else {
+    for (let i=n1; i<=n2; i++) {
+        sum += i 
+    } console.log("SUM", sum);
+}
